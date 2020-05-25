38ae8621429ba52f8162772a7e43455037430267
Yohey Ishizu <yohey.ishizu@gmail.com>
2020-05-25 23:21:59 +0900

    shiboken2: give up bottle, change dependencies

--- Formula/shiboken2.rb
+++ Formula/shiboken2.rb
@@ -5,29 +5,24 @@ class Shiboken2 < Formula
   version "5.11.2"
   head "http://code.qt.io/pyside/pyside-setup.git", :branch => "5.11"
 
-  bottle do
-    root_url "https://dl.bintray.com/freecad/bottles-freecad"
-    cellar :any
-    sha256 "fcc89486726d1a58d851c03671307b872a107a404f0765fece1275c703f55603" => :high_sierra
-    sha256 "f85f04b58aeeab7c4bdba96028f239ecfc9956ada699b47b48403264b4da28e3" => :sierra
-    sha256 "3761a858d70aad57b3d3b40fe5563c1068f8a6735fd3e5b7d4c2673e8af0364f" => :el_capitan
-  end
 
   depends_on "cmake" => :build
   depends_on "llvm"
   depends_on "numpy"
-  depends_on "qt"
+  depends_on "yohey/legacy/qt_5.11"
 
   option "without-python", "Build without python 2 support"
-  depends_on "python@2" => :recommended
-  depends_on "python3" => :optional
+  depends_on "python@3.8"
 
   def install
-    qt = Formula["qt"]
+    qt = Formula["yohey/legacy/qt_5.11"]
 
     ENV["LLVM_INSTALL_DIR"] = Formula["llvm"].opt_prefix
 
     Language::Python.each_python(build) do |python, version|
+      pyhome = `python#{version}-config --prefix`.chomp
+      python = "#{pyhome}/bin/python#{version}"
+
       mkdir "macbuild#{version}" do
         args = std_cmake_args
 
